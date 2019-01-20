787142d1a5f76f56296ec3c1467fdcc00038dac4
Markus Stöbe <markus@stoebe.me>
2019-01-20 11:48:23 +0100

    Initial Commit

---
+++ Network Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Network Chat
+//
+//  Created by Markus Stöbe on 20.01.19.
+//  Copyright © 2019 Markus Stöbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
